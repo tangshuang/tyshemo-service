5d2b798e21eb9d11224889435514603bab57a6f2
tangshuang <mail@tangshuang.net>
2021-07-02 14:53:58 +0800

    feat: supports guess and merge

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-const Service = require('./service')
+const Service = require('./lib/service')
+const { guess, merge } = require('./lib/guess')
 
 module.exports = Service
+module.exports.guess = guess
+module.exports.merge = merge
